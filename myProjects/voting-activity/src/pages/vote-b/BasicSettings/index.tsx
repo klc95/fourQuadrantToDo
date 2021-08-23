9926c358031b5f0987d0ab5a07ae1d66f6cd9812
klc <k57795357@163.com>
2021-08-23 23:58:52 +0800

    移动端页面预览

---
+++ myProjects/voting-activity/src/pages/vote-b/BasicSettings/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import MobliePreview from './MobilePagePreview'
+
+export default function BasicSettings() {
+
+  return (
+    <div>
+      <h1 className='BasicSettings'>
+          <MobliePreview />
+      </h1>
+    </div>
+  );
+}
+
